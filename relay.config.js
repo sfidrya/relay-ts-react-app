ab15405219b66eefd076394bb5b9219e64803826
Svetlana Fidrya <sf@zabiyaka.com>
2021-10-05 14:59:57 +0300

    Move relay-compiler keys to 'relay-config.js'

--- relay.config.js
+++ relay.config.js
@@ -2,6 +2,8 @@ module.exports = {
   // ...
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
   src: "./src",
-  schema: "./data/schema.graphql",
+  schema: "./schema.graphql",
+  language: "typescript", 
+  artifactDirectory: "./src/__generated__",
   exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**", "**/.yarn/**"],
 }
